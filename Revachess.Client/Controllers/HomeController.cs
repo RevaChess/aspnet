eb491a00073e135e2dd0c6cd05e369f09d34a15e
Akovi Mensah <akovi66@gmail.com>
2021-05-18 05:31:17 -0500

    connection to webapi

--- Revachess.Client/Controllers/HomeController.cs
+++ Revachess.Client/Controllers/HomeController.cs
@@ -1,12 +1,31 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace Medialab.Client.Controllers
 {
   [Route("[controller]")]
   public class HomeController : Controller
   {
-    public IActionResult Index()
+    public IConfiguration _configuration;
+    public HomeController(IConfiguration configuration)
     {
+      _configuration = configuration;
+    }
+    public async Task<IActionResult> Index()
+    {
+      var client = new HttpClient();
+      var response = await client.GetAsync($"{_configuration["Services:webapi"]}/game");
+      List<string> result = null;
+
+      if (response.IsSuccessStatusCode)
+      {
+        result = JsonConvert.DeserializeObject<List<string>>(await response.Content.ReadAsStringAsync());
+        ViewBag.Games = result;
+      }
       return View("index");
     }
   }
