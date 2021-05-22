552f1334a34a6a99ac653e98849be95f4739880d
Noah Leach <nleach2013@fau.edu>
2021-05-22 19:31:21 -0400

    Added simple links to navbar

--- Revachess.Client/Controllers/HomeController.cs
+++ Revachess.Client/Controllers/HomeController.cs
@@ -5,9 +5,18 @@ namespace Medialab.Client.Controllers
   [Route("[controller]")]
   public class HomeController : Controller
   {
+    [Route("")]
+    [Route("Home")]
+    [Route("Home/Index")]
     public IActionResult Index()
     {
       return View("index");
+    }
+
+    [Route("/register")]
+    public IActionResult Register()
+    {
+      return View("register");
     }
   }
 }
