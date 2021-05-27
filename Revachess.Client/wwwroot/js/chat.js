bcb0d5c2b05310ffe354728256983a39f76d5178
Akovi Mensah <abmensah@wisc.edu>
2021-05-27 17:22:55 -0500

    improved chat

--- Revachess.Client/wwwroot/js/chat.js
+++ Revachess.Client/wwwroot/js/chat.js
@@ -21,7 +21,7 @@ connection.start().then(function () {
 });
 
 document.getElementById("sendButton").addEventListener("click", function (event) {
-    var user = document.getElementById("userInput").value;
+    var user = document.getElementById("user").innerText;
     var message = document.getElementById("messageInput").value;
     connection.invoke("SendMessage", user, message).catch(function (err) {
         return console.error(err.toString());
